877754b603f3b1f283af60bbc6d2eb2549e8d150
Flávio Marques <flaviomarques2@hotmail.com>
2024-02-06 22:16:28 -0300

    inserir método GET e estilo nos componentes de card das ongs

--- src/components/OngsCadastradas/OngsCadastradas.tsx
+++ src/components/OngsCadastradas/OngsCadastradas.tsx
@@ -1,7 +1,40 @@
 import { Link } from 'react-router-dom'
 import './OngsCadastradas.css'
-import { FaArrowCircleLeft } from 'react-icons/fa'
+import { FaArrowCircleLeft, FaRegTrashAlt } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
+import { mensagemDeAlerta } from '../../utils/Alertas/MensagemDeSucesso'
+
+interface Ong {
+    name: string
+    imagem: string
+    proposito: string
+}
+
 export function OngsCadastradas() {
+    const [ongs, setOngs] = useState<Ong[]>([]);
+
+    useEffect(() => {
+        const fetchOngs = async () => {
+            try {
+                const response = await fetch('https://65c1f4b1f7e6ea59682a235d.mockapi.io/api/v1/ongs')
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar as ONGs');
+                }
+                const data = await response.json();
+                setOngs(data);
+            } catch (error) {
+                console.error('Erro', error)
+            }
+        };
+        fetchOngs();
+    }, []);
+
+    const excluirOng = () => {
+        mensagemDeAlerta();
+        //inserir a exclusão, caso o usuário decida excluir, ou anular.
+
+    }
+
     return (
         <main className="container">
             <div className="container_icone">
@@ -11,13 +44,18 @@ export function OngsCadastradas() {
                 </Link>
             </div>
             <h1>ONGs Cadastradas</h1>
-
-            <div className='ongs-cadastradas_card'>
-                <p className='ongs-cadastradas_p'>Nome: </p>
-                <p className='ongs-cadastradas_p'>Imagem: </p>
-                <p className='ongs-cadastradas_p'>Segmento: </p>
+            <div className='container-ongs'>
+                {ongs.map((ong, index) => (
+                    <div key={index} className='container-ongs_box'>
+                        <div className='containter-ongs_icone'>
+                            <FaRegTrashAlt onClick={excluirOng} className='container-ongs_lixeira' />
+                        </div>
+                        <h2>{ong.name}</h2>
+                        <img src={ong.imagem} alt={ong.name} className='img' />
+                        <p>{ong.proposito}</p>
+                    </div>
+                ))}
             </div>
-            
 
 
 
--- src/utils/Alertas/MensagemDeSucesso.tsx
+++ src/utils/Alertas/MensagemDeSucesso.tsx
@@ -9,4 +9,14 @@ export const mensagemDeSucesso = () => {
         })
     );
 }
+export const mensagemDeAlerta = () => {
+    return (
+        Swal.fire({
+            title: "Você tem certeza que deseja excluir essa ONG?",
+            text: "Os dados serão apagados permanentemente!",
+            icon: "question"
+        })
+    );
+}
 
+//existe alguma forma de melhorar esse alert?
